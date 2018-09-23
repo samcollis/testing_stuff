0dc0f61800e41c30442086771f8ec53d87db4954
Sam Collis <sam.collis@zapier.com>
2018-09-23 13:20:04 +1000

    first commit#

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+	url: 'mongodb://sam:sam-test@sam-test-api-shard-00-00-jgtv3.mongodb.net:27017,sam-test-api-shard-00-01-jgtv3.mongodb.net:27017,sam-test-api-shard-00-02-jgtv3.mongodb.net:27017/test?ssl=true&replicaSet=sam-test-api-shard-0&authSource=admin&retryWrites=true'
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+var db = require('./config/db')
+
+const app = express()
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+	if(err) return console.log(err)
+	db = database.db("sam-test-api")	
+	require('./app/routes')(app, db);
+	
+	app.listen(port, () => {
+	console.log('We are live on ' + port);
+	});
+})
+

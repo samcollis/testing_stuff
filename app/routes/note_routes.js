15b3c43b5ef2f85e1e5b1a59ee85573e16f3fc27
Sam Collis <sam.collis@zapier.com>
2018-12-08 12:39:29 +1100

    Added a new endpoint

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,9 +2,9 @@ var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function(app, db) {
 
- 	app.get('/notes/:id', (req, res) => {
-    	const id = req.params.id;
-    	const details = { '_id': new ObjectID(id) };
+ 	app.get('/notes/:title', (req, res) => {
+    	const title = req.params.title;
+    	const details = { 'title': new ObjectID(id) };
     	db.collection('notes').findOne(details, (err, item) => {
       		if (err) {
         		res.send({'error':'An error has occurred'});
@@ -30,7 +30,7 @@ module.exports = function(app, db) {
  	 });
 
  	app.get('/notes', (req, res) => {
- 		db.collection('notes').find({text: "placeholder_text"}).toArray(function(err, docs) {
+ 		db.collection('notes').find().toArray(function(err, docs) {
  			if (err) {
  				console.log('error: ' + err)
  			} else {
@@ -40,4 +40,17 @@ module.exports = function(app, db) {
  		})
 
  	})
+
+
+    app.put('/notes:title', (req, res) => {
+      const note = { text: req.body.body, title: req.body.title };
+      const noteToUpdate = db.collection('notes').find({ title: note.title })
+      notetoUpdate.insert(note, (err, result) => {
+          if (err) { 
+            res.send({ 'error': 'An error has occurred' }); 
+          } else {
+            res.send(result.ops[0]);
+          }
+      });
+   });
 };
